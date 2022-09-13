909b927f2faf8543561a61a922f03adf31109e08
luzhao <luzhaoya@qq.com>
2022-09-13 16:29:01 +0800

    姓名：陆昭；操作：更新了一个列表；

--- javascript/MyUtil1.js
+++ javascript/MyUtil1.js
@@ -14,3 +14,36 @@ function print(a) {
   alert(a)
   print_log(a)
 }
+
+//定义一个类
+class MyList {
+  arr = []
+  constructor() {
+  }
+  add(obj) {
+    arr.push(obj)
+  }
+  get(index) {
+    return arr[index]
+  }
+  getList() {
+    return arr;
+  }
+  remove_1() {
+    return arr.pop();
+  }
+  remove_2(index) {
+    return arr.splice(index, index+1)
+  }
+
+  clear() {
+    arr = []
+  }
+  size() {
+    return arr.length;
+  }
+
+  insert(index, obj) {
+    return arr.splice(index, 0, obj)
+  }
+}
