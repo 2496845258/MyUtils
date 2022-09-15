125b5754a0a468983fa589e1ca3b92a40738c854
luzhao <luzhaoya@qq.com>
2022-09-15 08:33:45 +0800

    姓名：陆昭；操作：js增加事件绑定功能；

--- javascript/MyUtil1.js
+++ javascript/MyUtil1.js
@@ -19,11 +19,72 @@ function print_Html_Css(name, msg) {
 function print_log(a) {
   console.log(a)
 }
-function myprint(a) {
+function myPrint(a) {
   alert(a)
   print_log(a)
 }
 
+function myGetNode(namem) {
+  return document.querySelector(name)
+}
+
+function myGetNodes(name) {
+  return document.querySelectorAll(name)
+}
+
+
+//单击
+function myMouseClickOne(a, b) {
+  a.addEventListener('click', b)
+}
+
+//双击
+function myMouseClickTwo(a, b) {
+  a.addEventListener('dblclick', b)
+}
+
+//长按
+function myMouseClickLast(a, b) {
+  a.addEventListener('mousedown', b)
+}
+
+//移入（true表示子集也触发）
+function myMouseEnter(a, b, c) {
+  if ( null == b || false === c ) {
+    //子集不触发
+    a.addEventListener('mouseenter', b)
+  } else {
+    //子集触发
+    a.addEventListener('mouseover', b)
+  }
+}
+
+//鼠标移出
+function myMouseOut(a, b, c) {
+  if ( null == b || false === c ) {
+    //子集不触发
+    a.addEventListener('mouseleave', b)
+  } else {
+    //子集触发
+    a.addEventListener('mouseout', b)
+  }
+}
+
+//内容改变事件
+function myChange(a, b) {
+  a.addEventListener('change', b)
+}
+
+
+//键盘按下
+function myKeyBoardDown(a, b) {
+  a.addEventListener('keydown', b)
+}
+
+//按键释放
+function myKeyBoardUp(a, b) {
+  a.addEventListener('keyup', b)
+}
 
 //定义一个类
 class MyList {
